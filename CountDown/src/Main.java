dce5a8828028b832f0019c010f3374ff713d1036
ASUS <ASUS@DESKTOP-FMBOSD6>
2023-03-24 17:24:38 +0700

    Testing commit for the second time

--- CountDown/src/Main.java
+++ CountDown/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		System.out.println("Hello world!!");
+		System.out.println("Did it work??? I hope so");
 
 	}
 
